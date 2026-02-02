36f5150d4b82440e8075d0aec86cea53ccd3af47
Edu <serranotrenado.e@gmail.com>
2026-02-02 23:26:12 +0100

    started tutorial/demo and minimal physics and jump mechanic

--- Assets/Scripts/TiledBackground.cs
+++ Assets/Scripts/TiledBackground.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class TiledBackground : MonoBehaviour
 {
+    [SerializeField] private Vector3 forcedScale = new Vector3 (3, 3, 0);
+    
     void Start()
     {
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
@@ -11,5 +13,11 @@ public class TiledBackground : MonoBehaviour
         float width = height * Screen.width / Screen.height;
 
         sr.size = new Vector2(width, height);
+    }
+
+    void Awake() { 
+    
+        transform.localScale = forcedScale;
+    
     }
 }
