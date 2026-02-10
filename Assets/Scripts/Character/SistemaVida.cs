7c8893f3c9a146cb17b3ee1e1d166555248d23cb
Eduu115 <serranotrenado.e@gmail.com>
2026-02-10 13:15:48 +0100

    fixed collision and getting damaged, TODO: UI

--- Assets/Scripts/Character/SistemaVida.cs
+++ Assets/Scripts/Character/SistemaVida.cs
@@ -7,7 +7,7 @@ public class SistemaVida : MonoBehaviour
 {
     [Header("Configuracion de vida")]
     [SerializeField] private int vidaMaxima = 3;
-    private int vidaActual;
+    [SerializeField] private int vidaActual;
 
     [Header("UI Corazones")]
     [SerializeField] private Image[] corazones;
@@ -28,66 +28,72 @@ public class SistemaVida : MonoBehaviour
         ActualizarCorazones();
     }
 
-    public void RecibirDaño(int cantidad) {
+    public void RecibirDaño(int cantidad)
+    {
         if (esInvencible) return;
 
-        //restamos vida
+        // restamos vida
         vidaActual -= cantidad;
-        vidaActual = Math.Clamp(vidaActual, 0, vidaMaxima);
+        vidaActual = Mathf.Clamp(vidaActual, 0, vidaMaxima);
 
         // mandamos actualizacion a la UI
         ActualizarCorazones();
 
-        if (scriptMovimiento != null){
+        if (scriptMovimiento != null)
+        {
             scriptMovimiento.RecibirGolpe();
         }
 
-        // iniciamos la corrutna para el tiempo/frames de invencibilidad
+        // iniciamos la corrutina para el tiempo/frames de invencibilidad
         StartCoroutine(Invencibilidad());
 
         // procesamos la muerte
-        if (vidaActual < 0) {
+        if (vidaActual <= 0)
+        {
             Morir();
         }
     }
 
-    void ActualizarCorazones(){
-        for (int i = 0; i < corazones.Length; i++){
-            if (i < vidaActual){
+    void ActualizarCorazones()
+    {
+        for (int i = 0; i < corazones.Length; i++)
+        {
+            if (i < vidaActual)
+            {
                 corazones[i].sprite = corazonLleno;
             }
-            else{
+            else
+            {
                 corazones[i].sprite = corazonVacio;
             }
 
         }
     }
 
-    // Corrutina de invencibilidada
-    IEnumerator Invencibilidad() {
+    // Corrutina de invencibilidad
+    IEnumerator Invencibilidad()
+    {
         esInvencible = true;
         yield return new WaitForSeconds(tiempoInvencible);  // Solo espera
         esInvencible = false;
     }
 
-    void Morir(){
+    void Morir()
+    {
         Debug.Log("El jugador ha muerto!");
-
-        // TODO: se podria añadir añadir:
-        // - Reiniciar nivel
-        // - Mostrar pantalla de Game Over
-        // - Desactivar controles
 
         // reiniciamos la escena después de 2 segundos
         Invoke("ReiniciarNivel", 2f);
     }
-    public void Curar(int cantidad){
+    public void Curar(int cantidad)
+    {
         vidaActual += cantidad;
         vidaActual = Mathf.Clamp(vidaActual, 0, vidaMaxima);
         ActualizarCorazones();
     }
 
-    void ReiniciarNivel() {
+    void ReiniciarNivel()
+    {
         UnityEngine.SceneManagement.SceneManager.LoadScene(
             UnityEngine.SceneManagement.SceneManager.GetActiveScene().name
         );
--- Assets/Scripts/Traps/StaticSawTrap.cs
+++ Assets/Scripts/Traps/StaticSawTrap.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,20 +10,43 @@ public class StaticSawTrap : MonoBehaviour
     [SerializeField] private int dañoQueCausa = 1;
     [SerializeField] private string tagJugador = "Player";
 
-    // Detectar colisión con el jugador
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Verificar si colisionó con el jugador
-        if (collision.gameObject.CompareTag(tagJugador))
-        {
-            // Obtener el script de vida del jugador
-            SistemaVida sistemaVida = collision.gameObject.GetComponent<SistemaVida>();
+    // Soporta tanto colisiones normales como triggers
+    void OnCollisionEnter2D(Collision2D collision) => HandleHit(collision.gameObject);
+    void OnTriggerEnter2D(Collider2D collider) => HandleHit(collider.gameObject);
 
-            // Si tiene el componente, causarle daño
-            if (sistemaVida != null)
+    private void HandleHit(GameObject other)
+    {
+        if (!other.CompareTag(tagJugador)) return;
+
+        // Buscar SistemaVida en el objeto, en sus padres o en sus hijos
+        SistemaVida sistemaVida = other.GetComponent<SistemaVida>()
+            ?? other.GetComponentInParent<SistemaVida>()
+            ?? other.GetComponentInChildren<SistemaVida>();
+
+        if (sistemaVida != null)
+        {
+            sistemaVida.RecibirDaño(dañoQueCausa);
+            return;
+        }
+
+        // Si no está directamente, intentar localizarlo vía el componente Character
+        Character character = other.GetComponent<Character>()
+            ?? other.GetComponentInParent<Character>()
+            ?? other.GetComponentInChildren<Character>();
+
+        if (character != null)
+        {
+            SistemaVida sv = character.GetComponent<SistemaVida>()
+                ?? character.GetComponentInParent<SistemaVida>()
+                ?? character.GetComponentInChildren<SistemaVida>();
+
+            if (sv != null)
             {
-                sistemaVida.RecibirDaño(dañoQueCausa);
+                sv.RecibirDaño(dañoQueCausa);
+                return;
             }
         }
+
+        Debug.LogWarning($"StaticSawTrap: jugador tocado pero no se encontró SistemaVida en '{other.name}'. Revisa el componente y la etiqueta '{tagJugador}'.", this);
     }
 }
